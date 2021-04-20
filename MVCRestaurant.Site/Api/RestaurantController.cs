e00490556cadd627f97940796e0aed5bca7531a2
Víctor Hugo Contreras <vcontreras@gmail.com>
2021-04-19 21:45:23 -0300

    Add Restaurant ApiController

---
+++ MVCRestaurant.Site/Api/RestaurantController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVCRestaurant.Data.Services;
+
+namespace MVCRestaurant.Site.Api
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly IRestaurantData _db;
+        public RestaurantController()
+        {
+            _db = new InMemoryRestaurantData();
+        }
+        [HttpGet]
+        public IEnumerable<MVCRestaurant.Data.Models.Restaurant> Get()
+        {
+            return _db.GetAll();
+        }
+    }
+}
